48665d98f561607f3eababe36ca03577e5821ca0
Thomas Hawley <thawley22@turing.edu>
2023-07-26 00:28:39 -0600

    feat: Add update action

--- app/controllers/api/v0/users/subscriptions_controller.rb
+++ app/controllers/api/v0/users/subscriptions_controller.rb
@@ -1,10 +1,17 @@
 class Api::V0::Users::SubscriptionsController < ApplicationController
-  before_action :find_subscription, only: [:create]
+  before_action :find_subscription, only: [:create, :update]
+  before_action :find_user_sub, only: [:update]
+
   def create
     user_subscription = UserSubscription.new(user_subscription_params)
     if user_subscription.save
       render json: { data: { message: "Successfully subscribed to #{@subscription.title}" } }, status: 200
     end
+  end
+
+  def update
+    @user_sub.update(status: 1)
+    render json: { data: { message: "Successfully cancelled #{@subscription.title} subscription"}}, status: 200
   end
 
   private
@@ -14,5 +21,9 @@ class Api::V0::Users::SubscriptionsController < ApplicationController
 
     def find_subscription
       @subscription = Subscription.find(params[:subscription_id])
+    end
+
+    def find_user_sub
+      @user_sub = UserSubscription.where(user_subscription_params)
     end
 end
