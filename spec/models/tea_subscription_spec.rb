e3e916b6ad2506d574ee122b8416e19b441e0299
Thomas Hawley <thawley22@turing.edu>
2023-07-25 18:57:55 -0600

    feat: Add relationships to models

--- spec/models/tea_subscription_spec.rb
+++ spec/models/tea_subscription_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TeaSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :tea }
+    it { should belong_to :subscription }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many :user_subscriptions }
+    it { should have_many(:subscriptions).through(:user_subscriptions) }
+  end
 end
