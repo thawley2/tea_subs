f684b95bc876f1dfb1d2e78cd849b05d33a0b1ce
Thomas Hawley <thawley22@turing.edu>
2023-07-25 21:26:47 -0600

    test: Add enum test

--- spec/models/user_subscription_spec.rb
+++ spec/models/user_subscription_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe UserSubscription, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
     it { should belong_to :subscription }
+    it { should define_enum_for(:status).with_values([:active, :cancelled])}
   end
 end
