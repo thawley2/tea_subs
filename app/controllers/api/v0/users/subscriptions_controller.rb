bc7b10b5333e53c36ef524ebffcf8401e9952cf7
Thomas Hawley <thawley22@turing.edu>
2023-07-25 21:43:15 -0600

    feat: Complete create action with proper response

--- app/controllers/api/v0/users/subscriptions_controller.rb
+++ app/controllers/api/v0/users/subscriptions_controller.rb
@@ -1,10 +1,18 @@
 class Api::V0::Users::SubscriptionsController < ApplicationController
+  before_action :find_subscription, only: [:create]
   def create
-    require 'pry'; binding.pry
-    UserSubscription.new
+    user_subscription = UserSubscription.new(user_subscription_params)
+    if user_subscription.save
+      render json: { data: { message: "Successfully subscribed to #{@subscription.title}" } }, status: 200
+    end
   end
 
-  def user_subscription_params
-    params.permit(:user_id, :subscription_id)
-  end
+  private
+    def user_subscription_params
+      params.permit(:user_id, :subscription_id)
+    end
+
+    def find_subscription
+      @subscription = Subscription.find(params[:subscription_id])
+    end
 end
