64c6cc18f24608c4b24d162e908e4985bb9dcf88
Thomas Hawley <tchawley2@gmail.com>
2023-07-26 22:54:26 -0600

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+2.times do 
+    FactoryBot.create(:user)
+end
+
+5.times do
+    FactoryBot.create(:subscription)
+end
+
+10.times do
+    FactoryBot.create(:tea)
+end
+
+3.times do
+    FactoryBot.create(:user_subscription, user_id: rand(1..2), subscription_id: rand(1..5), status: rand(0..1) )
+end
+
+6.times do
+    FactoryBot.create(:tea_subscription, tea_id: rand(1..10), subscription_id: rand(1..5))
+end
+
