4cfb8ea6c2082a0ee0583528561a6cbd72a4cef7
Thomas Hawley <tchawley2@gmail.com>
2023-07-26 22:28:40 -0600

    test: Add test for #all_subs method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe User, type: :model do
     it { should have_many :user_subscriptions }
     it { should have_many(:subscriptions).through(:user_subscriptions) }
   end
+
+  before(:each) do
+    test_data
+  end
+
+  describe '#all_subs' do
+    it 'returns a list of all user subscriptions' do
+      expect(@user1.all_subs.size).to eq(2)
+      expect(@user1.all_subs).to eq([@sub1, @sub2])
+      expect(@user1.all_subs.first.status).to eq(0)
+    end
+  end
 end
